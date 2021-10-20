7026206f972b87a39c63148d2f42c0e6fc97d4db
양효빈 <qls0786@naver.com>
2021-10-20 11:15:38 +0000

    lombok 추가 및 JPA 세팅

--- src/main/java/com/board/Application.java
+++ src/main/java/com/board/Application.java
@@ -1,12 +1,16 @@
 package com.board;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {JpaRepositoriesAutoConfiguration.class})
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Application {
-    
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-} 
+}
