6a912582905457f003d9b8e5f3bedc7b365f27ca
양효빈 <qls0786@naver.com>
2021-11-15 14:57:02 +0000

    코드 정리 / 로그인 기능 오류 수정중

--- src/main/java/com/board/controller/MemberController.java
+++ src/main/java/com/board/controller/MemberController.java
@@ -19,17 +19,12 @@ import com.board.service.MemberService;
 public class MemberController {
     
     private MemberService memberService;
-    private MemberRepository member;
     
     @Autowired
     public MemberController(MemberService memberService){
         this.memberService = memberService;
     }
-    
-   /* @Autowired
-    public MemberController(MemberRepository member){
-        this.member = member;
-    }*/
+
     
     @GetMapping("/members/new")
     public String createForm() {
@@ -39,16 +34,9 @@ public class MemberController {
     @PostMapping("/members/new")
     public String create(MemberForm form, Model msg){
         Member member = new Member();
-        //Optional<String> name = Optional.ofNullable(form.getName());
         member.setName(form.getName());
         member.setPassword(form.getPassword());
-            
-        System.out.println("***********************로그 출력********************");
-        System.out.println("form에 저장된 이름과 비밀번호 : "+form.getName()+form.getPassword());
-        
-        //System.out.println("member에 저장된 이름과 비밀번호 : "+member.getName()+member.getPassword());
-        
-            
+       
         if(memberService.join(member)==0L){ 
             msg.addAttribute("loginMessage", "이미 존재하는 회원입니다!"); 
             return "members/createMemberForm";
@@ -57,9 +45,9 @@ public class MemberController {
     }
      
     @PostMapping("/signIn")
-    public String signIn(String inputName, String inputPassword){
-        //Optional<Member> member = Optional.ofNullable(this.member.findByNameAndPassword(inputName, inputPassword));
+    public String signIn(String inputName, String inputPassword, Model msg){
         if(memberService.isMember(inputName, inputPassword) != null){
+            msg.addAttribute("loginMessage", inputName+"님 환영합니다!!"); 
             return "loginOK";
         }
         return "loginFail";
@@ -67,7 +55,6 @@ public class MemberController {
     
     @GetMapping("/members")
     public String list(Model model){
-        //Optional<List<Member>> members=Optional.ofNullable(memberService.findMembers());
         model.addAttribute("members",memberService.findMembers());
         return "members/memberList";
     }
