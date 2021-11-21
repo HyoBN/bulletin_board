b6ef7822ba6ef0a94532ef176879986305a3c687
양효빈 <qls0786@naver.com>
2021-11-21 13:50:13 +0000

    회원 이름 출력 추가

--- src/main/java/com/board/controller/HomeController.java
+++ src/main/java/com/board/controller/HomeController.java
@@ -30,7 +30,7 @@ public class HomeController{
         // 세션에 회원 데이터가 없으면 홈으로 이동
         if (loginMember != null) {
                 // 세션이 유지되면 로그인 홈으로 이동
-            model.addAttribute("loginMessage", /*loginMember.getName()+*/"님 로그인 상태입니다."); 
+            model.addAttribute("loginMessage", loginMember.getName()+"님 로그인 상태입니다."); 
             return "loginHome";
 
         }
