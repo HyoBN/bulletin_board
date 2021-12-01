0679a33388ef122e38c952fc797a8799d0eaa3de
양효빈 <qls0786@naver.com>
2021-12-01 13:09:34 +0000

    게시글 crud 뼈대 구축

---
+++ src/main/java/com/board/controller/PostController.java
@@ -0,0 +1,13 @@
+package com.board.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.ui.Model;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
---
+++ src/main/java/com/board/entity/Post.java
@@ -0,0 +1,11 @@
+package com.board.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+    
+}
