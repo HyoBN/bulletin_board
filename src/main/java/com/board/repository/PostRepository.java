59e5a4b0bc8b7cd5470c4368d8c9ef3a235e6669
양효빈 <qls0786@naver.com>
2021-12-01 13:51:23 +0000

    Spring Data Jpa 활용, 게시글 repo 구축

---
+++ src/main/java/com/board/repository/PostRepository.java
@@ -0,0 +1,18 @@
+package com.board.repository;
+
+import com.board.entity.Member;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+    
+@Repository
+public interface MemberRepository extends JpaRepository<Post, Long> {
+    Post save(Post post);
+    Optional<Post> findById(Long id);
+    Optional<Post> findByTitle(String title);
+    Optional<Post> findByWriter(String writer);
+    List<Post> findAll();
+
+}
